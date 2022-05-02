e5a8cc4ba51abc8540b79cf685d1cee9f892b26b
Alex Baroody <abaroody@princeton.edu>
2022-05-02 16:33:10 -0400

    add log statements

--- cache/lfu_test.go
+++ cache/lfu_test.go
@@ -27,11 +27,15 @@ import (
 /******************************************************************************/
 
 func TestGet(t *testing.T) {
+	// Log progress
+	fmt.Println("Testing Get...")
 	capacity := 64
 	lfu := NewLfu(capacity)
 	checkCapacity(t, lfu, capacity)
 
 	for i := 0; i < 4; i++ {
+		// Log progress
+		fmt.Printf("\tTesting Get with key: %d\n", i)
 		key := fmt.Sprintf("key%d", i)
 		val := []byte(key)
 		ok := lfu.Set(key, val)
@@ -49,11 +53,15 @@ func TestGet(t *testing.T) {
 }
 
 func TestRemove(t * testing.T) {
+	// Log progress
+	fmt.Println("Testing Remove...")
 	capacity := 64
 	lfu := NewLfu(capacity)
 	checkCapacity(t, lfu, capacity)
 
 	for i := 0; i < 4; i++ {
+		// Log progress
+		fmt.Printf("\tAdding key: %d\n", i)
 		key := fmt.Sprintf("key%d", i)
 		val := []byte(key)
 		ok := lfu.Set(key, val)
@@ -70,6 +78,8 @@ func TestRemove(t * testing.T) {
 	}
 
 	for i := 0; i < 4; i++ {
+		// Log progress
+		fmt.Printf("\tRemoving key: %d\n", i)
 		key := fmt.Sprintf("key%d", i)
 		refVal := []byte(key)
 		val, ok := lfu.Remove(key)
@@ -87,19 +97,29 @@ func TestRemove(t * testing.T) {
 }
 
 func TestEvictSimple(t *testing.T) {
+	// Log progress
+	fmt.Println("Testing Evict 1...")
 	capacity := 100
 	lfu := NewLfu(capacity)
 	checkCapacity(t, lfu, capacity)
 
 	// sets 0 thru 9
+
+	// Log progress
+	fmt.Println("\tSetting 0 thru 9...")
 	for i := 0; i < 10; i++ {
+		// Log progress
+		fmt.Printf("\t\tAdding key: %d. ", i)
 		key := fmt.Sprintf("____%d", i)
 		val := []byte(key)
 		ok := lfu.Set(key, val)
 		if !ok {
+			fmt.Printf("Failed to add binding with key: %s\n", key)
 			t.Errorf("Failed to add binding with key: %s", key)
 			t.FailNow()
 		}
+		// log progress
+		fmt.Print("Success! \n")
 	}
 
 	// 0: 1
@@ -114,13 +134,21 @@ func TestEvictSimple(t *testing.T) {
 	// 9: 1
 
 	// get 0 thru 8 
-	for i := 0; i < 9; i++ {
+	// Log progress
+	fmt.Println("\tGetting 0 thru 8...")
+	for i := 0; i < 10; i++ {
+		// Log progress
+		fmt.Printf("\t\tGetting key: %d. ", i)
 		key := fmt.Sprintf("____%d", i)
 		res, found := lfu.Get(key)
 		if !found {
+			// log progress
+			fmt.Printf("Failed to get binding with key: %s\n", key)
 			t.Errorf("Could not find %s as binding with key: %s", res, key)
 			t.FailNow()
 		}
+		// log progress
+		fmt.Printf("Got %s\n", res)
 	}
 
 	// 0: 2
@@ -135,6 +163,8 @@ func TestEvictSimple(t *testing.T) {
 	// 9: 1
 
 	// set 10
+	// Log progress
+	fmt.Print("\tSetting 10... ")
 	key := fmt.Sprintf("___10")
 	val := []byte("____a")
 	ok := lfu.Set(key, val)
@@ -142,6 +172,7 @@ func TestEvictSimple(t *testing.T) {
 		t.Errorf("Failed to add binding with key: %s", key)
 		t.FailNow()
 	}
+	fmt.Printf("Added %s.\n", val)
 
 	// 0: 2
 	// 1: 2
@@ -156,7 +187,11 @@ func TestEvictSimple(t *testing.T) {
 
 
 	// gets 0 thru 10, 9 should not get cache hit
+	// Log progress
+	fmt.Println("\tGetting 0 thru 10...")
 	for i := 0; i <= 10; i++ {
+		// Log progress
+		fmt.Printf("\t\tGetting key: %d. ", i)
 		key = fmt.Sprintf("____%d", i)
 		if i == 10 {
 			key = fmt.Sprintf("___10")
@@ -164,22 +199,36 @@ func TestEvictSimple(t *testing.T) {
 		res, found := lfu.Get(key)
 		// fmt.Printf("%s: %s. %t\n", key, res, found)
 		if found && i == 9 {
+			fmt.Printf("Failed to get binding with key: %s\n", key)
 			t.Errorf("Found %s as binding with key: %s", res, key)
 			t.FailNow()
 		} else if !found && i != 9 {
+			fmt.Printf("Failed to get binding with key: %s\n", key)
 			t.Errorf("Could not find %s as binding with key: %s", res, key)
 			t.FailNow()
+		}
+		// log progress
+		if len(res) == 0 {
+			fmt.Print("Got nothing.\n")
+		} else {
+			fmt.Printf("Got %s.\n", res)
 		}
 	}
 }
 
 func TestEvict(t *testing.T) {
+	// Log progress
+	fmt.Println("Testing Evict...")
 	capacity := 100
 	lfu := NewLfu(capacity)
 	checkCapacity(t, lfu, capacity)
 
 	// sets 0 thru 9
+	// Log progress
+	fmt.Println("\tSetting 0 thru 9...")
 	for i := 0; i < 10; i++ {
+		// Log progress
+		fmt.Printf("\t\tAdding key: %d. ", i)
 		key := fmt.Sprintf("____%d", i)
 		val := []byte(key)
 		ok := lfu.Set(key, val)
@@ -187,6 +236,7 @@ func TestEvict(t *testing.T) {
 			t.Errorf("Failed to add binding with key: %s", key)
 			t.FailNow()
 		}
+		fmt.Print("Success! \n")
 	}
 
 	// 0: 1
@@ -201,14 +251,19 @@ func TestEvict(t *testing.T) {
 	// 9: 1
 
 	// get 0 thru 9 
+	// Log progress
+	fmt.Println("\tGetting 0 thru 9 a variable number of times...")
 	for i := 0; i < 10; i++ {
 		for j := 0; j <= i; j++ {
+			// Log progress
+			fmt.Printf("\t\tGetting key: %d. ", i)
 			key := fmt.Sprintf("____%d", i)
 			res, found := lfu.Get(key)
 			if !found {
 				t.Errorf("Could not find %s as binding with key: %s", res, key)
 				t.FailNow()
 			}
+			fmt.Printf("Got %s.\n", res)
 		}
 	}
 
@@ -224,6 +279,8 @@ func TestEvict(t *testing.T) {
 	// 9: 11
 
 	// set 10
+	// Log progress
+	fmt.Print("\tSetting 10... ")
 	key := fmt.Sprintf("___10")
 	val := []byte("____a")
 	ok := lfu.Set(key, val)
@@ -231,6 +288,7 @@ func TestEvict(t *testing.T) {
 		t.Errorf("Failed to add binding with key: %s", key)
 		t.FailNow()
 	}
+	fmt.Printf("Added %s.\n", val)
 
 	// 1: 3
 	// 2: 4
@@ -245,7 +303,11 @@ func TestEvict(t *testing.T) {
 
 
 	// gets 0 thru 10, 0 should not get cache hit
+	// Log progress
+	fmt.Println("\tGetting 0 thru 10...")
 	for i := 0; i <= 10; i++ {
+		// Log progress
+		fmt.Printf("\t\tGetting key: %d. ", i)
 		key = fmt.Sprintf("____%d", i)
 		if i == 10 {
 			key = fmt.Sprintf("___10")
@@ -258,6 +320,11 @@ func TestEvict(t *testing.T) {
 		} else if !found && i != 0 {
 			t.Errorf("Could not find %s as binding with key: %s", res, key)
 			t.FailNow()
+		}
+		if len(res) == 0 {
+			fmt.Print("Got nothing.\n")
+		} else {
+			fmt.Printf("Got %s.\n", res)
 		}
 	}
 }
